7d01e7a7f35e258ff360368af8e0185a6f064b57
YannickMontes <montes.yannick@gmail.com>
2025-10-06 19:07:08 +0200

    Update player with characterbody

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,29 +1,35 @@
-extends Node3D
+class_name Player
+
+extends CharacterBody3D
+
 var healthbar
-@export var move_speed:float = 5
+@export var move_speed:float = 15
 @export var health: int = 3
 
-var move_inputs: Vector2
+var move_inputs: Vector3
 
 func _ready() -> void:
-	healthbar = $RigidBody3D/SubViewport/HealthBar
+	healthbar = $SubViewport/HealthBar
 	healthbar.max_value = health
 
 func _process(delta:float) -> void:
 	if Input.is_action_just_pressed("damage_player"):
-		health -= 1
-		healthbar.update(health)
+		damage_player()
 
 func _physics_process(delta: float) -> void:
 	read_move_inputs()
 	move_inputs *= move_speed * delta
-	if move_inputs != Vector2.ZERO:
-		global_position += Vector3(move_inputs.x, 0.0, move_inputs.y)
+	if move_inputs != Vector3.ZERO:
+		velocity = move_inputs * move_speed
+		move_and_slide()
 	return
 
 func read_move_inputs():
 	move_inputs.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	move_inputs.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	move_inputs.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	move_inputs = move_inputs.normalized()
-	print(move_inputs)
 	return
+	
+func damage_player():
+	health -= 1
+	healthbar.update(health)
